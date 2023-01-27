933a8afb350e2dd1561faff49c8c382971f46316
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-01-27 08:07:09 +0700

    Update and rename sod.py to salient_object_detection.py

--- models/salient_object_detection.py
+++ models/salient_object_detection.py
@@ -15,44 +15,50 @@ class SOD(tf.keras.Model):
         self.image_size = image_size
         self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
         
-    def compile(self, optimizer, loss, metrics, **kwargs):
+    def compile(self, optimizer, loss, metrics=None, **kwargs):
         super(SOD, self).compile(**kwargs)
         self.optimizer = optimizer
-        self.loss = loss
+        self.loss_object = loss
         self.list_metrics = metrics
 
     @property
     def metrics(self):
-        return [
-            self.total_loss_tracker,
-            *self.list_metrics,
-        ]
+        if self.list_metrics:
+            return [
+                self.total_loss_tracker,
+                *self.list_metrics,
+            ]
+        else:
+            return [self.total_loss_tracker]
 
     def train_step(self, data):
-        for metric in self.list_metrics:
-            metric.reset_state()
         images, masks = data        
         with tf.GradientTape() as tape:
-            y_pred      = self.architecture(images, training=True)
-            loss_value  = self.loss(y_true=masks, y_pred=y_pred)
+            model_result = self.architecture(images, training=True)
+            y_pred       = model_result['saliency']
+            loss_value   = self.architecture.calc_loss(y_true=masks, y_pred=y_pred, loss_object=self.loss_object)
         grads = tape.gradient(loss_value, self.architecture.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.architecture.trainable_variables))
-        self.total_loss_tracker.update_state(loss_value)
         
-        for metric in self.list_metrics:
-            metric.update_state(masks, y_pred[0])
+        self.total_loss_tracker.update_state(loss_value)
+        if self.list_metrics:
+            for metric in self.list_metrics:
+                metric.reset_state()
+                metric.update_state(labels, y_pred)
         results = {m.name: m.result() for m in self.metrics}
         return results
 
     def test_step(self, data):
-        for metric in self.list_metrics:
-            metric.reset_state()
         images, masks = data
-        y_pred      = self.architecture(images, training=False)
-        loss_value  = self.loss(y_true=masks, y_pred=y_pred)
+        model_result = self.architecture(images, training=False)
+        y_pred       = model_result['saliency']
+        loss_value   = self.architecture.calc_loss(y_true=masks, y_pred=y_pred, loss_object=self.loss_object)
+        
         self.total_loss_tracker.update_state(loss_value)
-        for metric in self.list_metrics:
-            metric.update_state(masks, y_pred[0])
+        if self.list_metrics:
+            for metric in self.list_metrics:
+                metric.reset_state()
+                metric.update_state(labels, y_pred)
         results = {m.name: m.result() for m in self.metrics}
         return results
 
@@ -65,8 +71,8 @@ class SOD(tf.keras.Model):
 
     @tf.function
     def predict(self, inputs):
-        d0, d1, d2, d3, d4, d5, d6 = self.architecture(inputs, training=False)
-        return d0
+        pred = self.architecture.predict(inputs)
+        return pred
 
     def save_weights(self, weight_path, save_format='tf', **kwargs):
         self.architecture.save_weights(weight_path, save_format=save_format, **kwargs)
@@ -98,7 +104,6 @@ class SOD(tf.keras.Model):
                 "architecture": self.architecture,
                 "image_size": self.image_size,
                 "total_loss_tracker": self.total_loss_tracker,
-                # "tar_loss_tracker": self.tar_loss_tracker
             })
         return config
 
